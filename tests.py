b67063317d3d9ea5a93d126e550fdefff5966b11
majeedtanveer <majeed.tanveer@rwth-aachen.de>
2026-05-05 16:52:03 +0200

    add up tests about n k d and clean up

--- tests.py
+++ tests.py
@@ -1,22 +1,19 @@
 from simulation import RepetitionCodeBuilder
 from math import comb
 
-# Distance == min weight stabilizers
+def quantum_hamming_bound(n, k, d, num_P=3):
+    # num_P = number of correctable Pauli Errors usually 3. In the case of the repetition code it is 1
+    t = (d - 1) // 2
+    lhs = sum(comb(n, j) * (num_P)**j for j in range(t + 1)) * 2**k
+    rhs = 2**n
+    return lhs <= rhs
 
 def test_1():
     circuit_builder = RepetitionCodeBuilder()
     for d in [3, 5, 7, 9]:
         circuit = circuit_builder.build_circuit(distance=d)
         dem = circuit.detector_error_model()
-        print(dem.num_detectors+dem.num_observables)
-
-test_1()
-
-def quantum_hamming_bound(n, k, d):
-    t = (d - 1) // 2
-    lhs = sum(comb(n, j) * 3**j for j in range(t + 1)) * 2**k
-    rhs = 2**n
-    return lhs <= rhs
+        assert dem.num_detectors ==  circuit.num_qubits - dem.num_observables
 
 def test_2():
     circuit_builder = RepetitionCodeBuilder()
@@ -25,4 +22,21 @@ def test_2():
         dem = circuit.detector_error_model()
         n = circuit.num_qubits
         k = dem.num_observables
-        assert quantum_hamming_bound(n,k,d)
+
+        # repetition code only corrects one Pauli Error
+        assert quantum_hamming_bound(n,k,d,num_P=1)
+
+def test_3():
+    circuit_builder = RepetitionCodeBuilder()
+    circuit = circuit_builder.build_circuit()    
+    for d in [3, 5, 7, 9]:
+        circuit = circuit_builder.build_circuit(distance=d)    
+        dem = circuit.detector_error_model()
+    err = dem.shortest_graphlike_error()
+    distance = len(err)
+
+    assert distance == d
+
+test_1()
+test_2()
+test_3()
